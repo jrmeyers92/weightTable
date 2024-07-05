574af4b8a3eecef4164ceff41771e82273f4086a
Jake Meyers <jakermeyers92@gmail.com>
2024-07-05 12:41:01 -0500

    add gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleTagManager } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -16,6 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="G-G5MY6MJMD7" />
+
       <body className={inter.className}>{children}</body>
     </html>
   );
